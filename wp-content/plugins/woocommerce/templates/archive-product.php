e4ec956f6d9c28310bcccf8c9a839c1a0d7a5eb4
Victor Pinaev <akvalang70@gmail.com>
2016-03-16 17:33:20 +0400

    del sidebar

--- wp-content/plugins/woocommerce/templates/archive-product.php
+++ wp-content/plugins/woocommerce/templates/archive-product.php
@@ -92,7 +92,7 @@ get_header( 'shop' ); ?>
 		 *
 		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 		 */
-		do_action( 'woocommerce_after_main_content' );
+		//do_action( 'woocommerce_after_main_content' );
 	?>
 
 	<?php
@@ -101,7 +101,7 @@ get_header( 'shop' ); ?>
 		 *
 		 * @hooked woocommerce_get_sidebar - 10
 		 */
-		do_action( 'woocommerce_sidebar' );
+		//do_action( 'woocommerce_sidebar' );
 	?>
 
 <?php get_footer( 'shop' ); ?>
